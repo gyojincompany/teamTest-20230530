c40b72bc0e7d4dd00318ade96e6607277b697a47
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-30 11:21:29 +0900

    test

---
+++ src/main/java/com/gyojincompany/thymeleaf/ThymeleafTest20230504Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTest20230504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTest20230504Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/thymeleaf/ThymeleafTest20230504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTest20230504ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
